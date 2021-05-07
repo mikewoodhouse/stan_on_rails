69f6263298d6100ab3afb7becfa0b1e67a74c38a
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-07 16:23:33 +0100

    player knows how to say its name

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,6 @@
 class Player < ApplicationRecord
   def name()
-    return "#{self.surname}, #{self.firstname}" if self.firstname
-    return "#{self.surname}, #{self.initial}" if self.initial
-    return self.surname
+    return self.surname + (self.firstname ? ", " + self.firstname :
+             +(self.initial ? ", " + self.initial : ""))
   end
 end
