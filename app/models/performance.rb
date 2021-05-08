a0253be33944714d7f52d7dbecbc8b9e316111a2
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-08 18:00:43 +0100

    calculate bat/bowl averages

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,18 +2,18 @@ class Performance < ApplicationRecord
   belongs_to :player, foreign_key: true, optional: true
 
   def high_score
-    self.highest.to_s + (self.highestnotout ? "*" : "")
+    highest.to_s + (highestnotout ? "*" : "")
   end
 
   def bat_avg
-    0.0
+    return innings > notout ? (runsscored.to_f / (innings - notout)).round(2) : ""
   end
 
   def bowl_avg
-    99.99
+    return wickets > 0 ? (runs.to_f / wickets).round(2) : ""
   end
 
   def overs_and_balls
-    "#{self.overs}.#{self.balls}"
+    "#{overs}.#{balls}"
   end
 end
