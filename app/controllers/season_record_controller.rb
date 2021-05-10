3d9f20a911c498cba50c14fee4359fb0b9f06507
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-10 21:04:30 +0100

    cleaned up season record view, small amounts of rudimentary styling

--- app/controllers/season_record_controller.rb
+++ app/controllers/season_record_controller.rb
@@ -1,5 +1,9 @@
 class SeasonRecordController < ApplicationController
   def index
-    @season_records = SeasonRecord.all
+    @season_records = Hash.new { |h, year| h[year] = { 'TCC': nil, 'Opp': nil } }
+    puts @season_records
+    SeasonRecord.all.each do |ssn_rec|
+      @season_records[ssn_rec.year][ssn_rec.club] = ssn_rec
+    end
   end
 end
--- app/models/season_record.rb
+++ app/models/season_record.rb
@@ -1,2 +1,9 @@
 class SeasonRecord < ApplicationRecord
+  def highest_match
+    "#{highest} for #{highestwkts}, #{highestopps}, #{highestdate}"
+  end
+
+  def lowest_match
+    "#{lowest} for #{lowestwkts}, #{lowestopps}, #{lowestdate}"
+  end
 end
