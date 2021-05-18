b23f17e4bece58bdd34eb7fc408776729f39d304
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-18 20:10:50 +0100

    tweaked season record formatting/display

--- app/models/season_record.rb
+++ app/models/season_record.rb
@@ -1,9 +1,13 @@
 class SeasonRecord < ApplicationRecord
   def highest_match
-    "#{highest} for #{highestwkts}, #{highestopps}, #{highestdate}"
+    "#{highest} for #{highestwkts}, #{highestopps}, #{to_mmmdd(highestdate)}"
   end
 
   def lowest_match
-    "#{lowest} for #{lowestwkts}, #{lowestopps}, #{lowestdate}"
+    "#{lowest} for #{lowestwkts}, #{lowestopps}, #{to_mmmdd(lowestdate)}"
+  end
+
+  def to_mmmdd(dt)
+    dt ? dt.strftime("%b %e") : nil
   end
 end
