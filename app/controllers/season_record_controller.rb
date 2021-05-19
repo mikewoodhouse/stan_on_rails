56debe80604f3c7f277d401c4e791b474f295b73
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-19 10:51:55 +0100

    remove inadvertent puts statement

--- app/controllers/season_record_controller.rb
+++ app/controllers/season_record_controller.rb
@@ -1,7 +1,6 @@
 class SeasonRecordController < ApplicationController
   def index
     @season_records = Hash.new { |h, year| h[year] = { 'TCC': nil, 'Opp': nil } }
-    puts @season_records
     SeasonRecord.all.each do |ssn_rec|
       @season_records[ssn_rec.year][ssn_rec.club] = ssn_rec
     end
