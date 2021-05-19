37804c80b0c9dbd4ac7dde8b9a0d000d3d2b0ad0
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-19 10:53:02 +0100

    batting incremental improvements * extracted sql to separate file * min innings filter on career averages

--- app/controllers/batting_controller.rb
+++ app/controllers/batting_controller.rb
@@ -1,61 +1,16 @@
+require "batting_sql"
+
 class BattingController < ApplicationController
   def hundred_plus
     @hundreds = HundredPlus.includes(:player).all.order("score DESC")
   end
 
   def most_runs
-    @run_makers = Player.find_by_sql(
-      %{
-        SELECT
-          p.surname
-        , p.initial
-        , p.firstname
-        , p.code
-        , Sum(b.runsscored) runs
-        FROM
-          players p
-            INNER JOIN
-              performances b ON b.player_id = p.id
-        GROUP BY
-          p.surname
-        , p.initial
-        , p.firstname
-        , p.code
-        ORDER BY runs DESC
-      }
-    )
+    @run_makers = Player.find_by_sql TOTAL_RUNS_SQL
   end
 
   def averages
-    @player_bat_aves = Player.find_by_sql(
-      %{
-        SELECT
-          p.surname
-        , p.initial
-        , p.firstname
-        , p.code
-        , Sum(b.matches) matches
-        , Sum(b.innings) innings
-        , Sum(b.notout) notout
-        , Max(b.highest) highest
-        , Sum(runsscored) runsscored
-        , CASE Sum(b.innings)
-            WHEN Sum(b.notout) THEN 0.0
-            ELSE Sum(Cast(b.runsscored AS REAL)) / (Sum(b.innings) - Sum(b.notout))
-          END batave
-        FROM
-          players p
-            INNER JOIN
-              performances b ON b.player_id = p.id
-        GROUP BY
-          p.surname
-        , p.initial
-        , p.firstname
-        , p.code
-        HAVING
-          Sum(b.innings) - Sum(b.notout) >= 10
-        ORDER BY batave DESC
-      }
-    )
+    min_innings = params[:min_innings].to_i
+    @player_bat_aves = Player.find_by_sql [BAT_AVE_SQL, { :min_innings => min_innings }]
   end
 end
---
+++ lib/batting_sql.rb
@@ -0,0 +1,47 @@
+TOTAL_RUNS_SQL = %{
+  SELECT
+    p.surname
+  , p.initial
+  , p.firstname
+  , p.code
+  , Sum(b.runsscored) runs
+  FROM
+    players p
+      INNER JOIN
+        performances b ON b.player_id = p.id
+  GROUP BY
+    p.surname
+  , p.initial
+  , p.firstname
+  , p.code
+  ORDER BY runs DESC
+}
+
+BAT_AVE_SQL = %{
+  SELECT
+    p.surname
+  , p.initial
+  , p.firstname
+  , p.code
+  , Sum(b.matches) matches
+  , Sum(b.innings) innings
+  , Sum(b.notout) notout
+  , Max(b.highest) highest
+  , Sum(runsscored) runsscored
+  , CASE Sum(b.innings)
+      WHEN Sum(b.notout) THEN 0.0
+      ELSE Sum(Cast(b.runsscored AS REAL)) / (Sum(b.innings) - Sum(b.notout))
+    END batave
+  FROM
+    players p
+      INNER JOIN
+        performances b ON b.player_id = p.id
+  GROUP BY
+    p.surname
+  , p.initial
+  , p.firstname
+  , p.code
+  HAVING
+    Sum(b.innings) - Sum(b.notout) >= :min_innings
+  ORDER BY batave DESC
+}
