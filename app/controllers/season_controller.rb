cfe0b6d1769ba317df914e1c24afd875a26c46f2
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-05-23 17:42:56 +0100

    added season bowling averages

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -20,9 +20,13 @@ class SeasonController < ApplicationController
       },
       { :year => @year },
     ]
-    @perfs = {
+    @bat_perfs = {
       included: rows.select { |perf| perf.innings - perf.notout >= 5 }.sort_by { |perf| -perf.bat_avg },
       also: rows.select { |perf| perf.innings - perf.notout < 5 }.sort_by { |perf| perf.surname },
+    }
+    @bowl_perfs = {
+      included: rows.select { |perf| perf.wickets >= 10 }.sort_by { |perf| perf.bowl_avg },
+      also: rows.select { |perf| perf.wickets < 10 && perf.overs + perf.balls > 0 }.sort_by { |perf| perf.surname },
     }
   end
 end
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,11 +2,21 @@ class Performance < ApplicationRecord
   belongs_to :player, foreign_key: true, optional: true
 
   def bat_avg
-    return innings > notout ? (runsscored.to_f / (innings - notout)).round(2) : ""
+    innings > notout ? (runsscored.to_f / (innings - notout)).round(2) : ""
   end
 
   def bowl_avg
-    return wickets > 0 ? (runs.to_f / wickets).round(2) : ""
+    wickets > 0 ? (runs.to_f / wickets).round(2) : ""
+  end
+
+  def strike_rate
+    return "" if wickets == 0
+    (overs * 6 + balls) / wickets.to_f
+  end
+
+  def economy
+    return "" if overs + balls == 0
+    runs.to_f * 6 / (overs * 6 + balls)
   end
 
   def overs_and_balls
