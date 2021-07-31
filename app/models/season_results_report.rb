a52896d676c61dec6d1f1c722aa3a86db79c4005
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-07-31 16:07:42 +0100

    simple totals in season results

--- app/models/season_results_report.rb
+++ app/models/season_results_report.rb
@@ -1,6 +1,6 @@
 class SeasonResultsReport < Report
   def initialize
-    @title = "Season Results"
+    @title = "Result Summary - Season by Season"
     @columns = [
       Field.new("year", "Year", "year"),
       Field.new("played", "Played", "number"),
@@ -14,9 +14,28 @@ class SeasonResultsReport < Report
 
   def sql
     %{
-      SELECT *
+      SELECT
+        year
+      , played
+      , won
+      , lost
+      , drawn
+      , tied
+      , noresult
+      , 0 is_totals
       FROM seasons
-      ORDER BY year
+      UNION
+      SELECT
+        'Total' year
+      , Sum(played) played
+      , Sum(won) won
+      , Sum(lost) lost
+      , Sum(drawn) drawn
+      , Sum(tied) tied
+      , Sum(noresult) noresult
+      , 1 is_totals
+      FROM seasons
+      ORDER BY is_totals DESC, year DESC
     }
   end
 end
