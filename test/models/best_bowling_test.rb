5ad8f5c413a5d5e3ad7bb65d56f541b62d231888
Mike Woodhouse <mikewoodhouse@gmail.com>
2021-08-13 14:36:11 +0100

    clean out unused/unwanted/unloved items relating to defunct models

--- test/models/best_bowling_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BestBowlingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/season_record_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SeasonRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
