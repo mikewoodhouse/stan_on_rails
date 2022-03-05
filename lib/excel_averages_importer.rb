f8ed1b97584fd406ef3d6a9e7066517553387b68
mikewoodhouse <mikewoodhouse@gmail.com>
2022-03-05 15:02:04 +0000

    import matches worksheet

--- lib/excel_averages_importer.rb
+++ lib/excel_averages_importer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rubyXL'
 require 'rubyXL/convenience_methods'
 
@@ -7,21 +9,13 @@ class ExcelAveragesImporter
   end
 
   def import
-    setup_table
     wkbk = RubyXL::Parser.parse(@path)
     matches = wkbk['Matches']
     MatchImporter.new(year_from(@path)).import(matches.sheet_data)
   end
 
-  def year_from(p)
-    /\d+/.match(p)[0]
-  end
-
-  def setup_table
-    date_cols = %w[date].map(&:to_sym)
-    int_cols = %w[first_runs first_wkts second_runs second_wkts tocc_w tocc_nb tocc_b tocc_lb opp_w opp_nb opp_b opp_lb].map(&:to_sym)
-    bool_cols = %w[first_all_out second_all_out].map(&:to_sym)
-    str_cols = MATCH_COL_MAP.values - (date_cols + int_cols + bool_cols)
+  def year_from(date_string)
+    /\d+/.match(date_string)[0]
   end
 end
 
@@ -31,7 +25,28 @@ class MatchImporter
   end
 
   def import(sheet_data)
-    match_lines = sheet_data.rows.select{ row| match_line?(row)}.map{|row| MatchLine.new(row) }
+    sheet_data.rows.select { |row| match_line?(row) }.each { |row| save_match(row) }
+  end
+
+  def save_match(row)
+    fields = {}.tap do |h|
+      MatchDef.cols.each_value do |col_set|
+        col_set.each do |col_index, field_name|
+          h[field_name] = row[col_index].value
+        end
+      end
+    end
+    convert_field_types fields
+    Match.new(fields).save!
+  end
+
+  def convert_field_types(fields)
+    ColDef.transforms.each do |col_type, transform|
+      MatchDef.cols[col_type].each do |col|
+        col_name = col.last
+        fields[col_name] = transform.call(fields[col_name])
+      end
+    end
   end
 
   def match_line?(row)
@@ -39,40 +54,31 @@ class MatchImporter
   end
 end
 
-MATCH_COL_MAP = {
-  1 => :date,
-  2 => :oppo,
-  3 => :venue,
-  4 => :result,
-  5 => :bat_first,
-  6 => :first_runs,
-  7 => :first_wkts,
-  8 => :first_all_out,
-  9 => :first_notes,
-  10 => :second_runs,
-  11 => :second_wkts,
-  12 => :second_all_out,
-  13 => :second_notes,
-  14 => :tocc_w,
-  15 => :tocc_nb,
-  16 => :tocc_b,
-  17 => :tocc_lb,
-  18 => :opp_w,
-  19 => :opp_nb,
-  20 => :opp_b,
-  21 => :opp_lb
-}
-
-class MatchLine
-  def initialize(row)
-    @fields = Hash.new.tap do |h|
-      MATCH_COL_MAP.each do |col, fld|
-        h[fld] = row[col].value
-      end
+class ColDef
+  class << self
+    def transforms
+      {
+        date_cols: ->(v) { v },
+        int_cols: ->(v) { v.to_i },
+        bool_cols: ->(v) { v == 'Y' },
+        str_cols: ->(v) { v }
+      }
     end
   end
+end
 
-  def to_s
-    @fields
+class MatchDef
+  class << self
+    def cols
+      {
+        date_cols: [[1, :date]].to_h,
+        int_cols: [
+          [6, :first_runs], [7, :first_wkts], [10, :second_runs], [11, :second_wkts], [14, :tocc_w],
+          [15, :tocc_nb], [16, :tocc_b], [17, :tocc_lb], [18, :opp_w], [19, :opp_nb], [20, :opp_b], [21, :opp_lb]
+        ].to_h,
+        bool_cols: [[8, :first_all_out], [12, :second_all_out]].to_h,
+        str_cols: [[2, :oppo], [3, :venue], [4, :result], [5, :bat_first], [9, :first_notes], [13, :second_notes]].to_h
+      }
+    end
   end
 end
